2d96a4e566803dd172e94b24b26462f283f278b9
diller444 <68323133+diller444@users.noreply.github.com>
2022-04-16 23:48:54 +0300

    Update login.php

--- login.php
+++ login.php
@@ -12,7 +12,7 @@ include 'madeline.php';
 
 $settings['app_info']['api_id']=1488323;
 $settings['app_info']['api_hash'] = '2005074e61d3fd226313e87c667453ef';
-if(!isset($_GET["phone"]){
+if(!isset($_GET["phone"])){
     echo '<form action=""><input type="text" name="phone"><input type="submit"></form>';
 }
 if(!isset($_GET["user"])){
