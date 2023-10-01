8d690ad985f416568998f01097f4390e8baf07c3
Shinovon <arman200711@gmail.com>
2023-10-01 19:34:56 +0500

    Add donation links in about

--- about.php
+++ about.php
@@ -14,28 +14,35 @@ echo Themes::head();
 echo '</head>';
 echo Themes::bodyStart();
 echo MP::x('<div><a href="login.php">'.$lng['back'].'</a></div>');
-echo '<h1>MPGram Web</h1>';
-echo MP::x('<p>MPGram Web (aka MIDletPascalGram Web) is lightweight telegram web client based on MadelineProto library, for devices with internet access and basic HTML & CSS support</p>');
-echo MP::x('<p>Links:<br>');
-echo '<a href="https://github.com/shinovon/mpgram-web">GitHub</a><br>';
+
+?>
+<h1>MPGram Web</h1>
+<p>MPGram Web (aka MIDletPascalGram Web) is lightweight telegram web client based on MadelineProto library, for devices with internet access and basic HTML & CSS support</p>
+<p>Links:<br>
+<a href="https://github.com/shinovon/mpgram-web">GitHub</a><br>
+<?php
 if(MP::getUser()) {
 	echo '<a href="chat.php?c=nnmidletschat">Discussion chat</a>';
 } else {
 	echo '<a href="https://t.me/nnmidletschat">Discussion chat</a>';
 }
-echo '</p>';
-echo MP::x('<p>Developers:<br>');
-echo '<b>Shinovon</b> <a href="https://github.com/shinovon">github</a>';
-echo ' <a href="https://t.me/shinovon">t.me</a>';
-echo '<br>';
-echo MP::x('<b>MuseCat77</b></i>');
-echo ' <a href="https://github.com/musecat77">github</a>';
-echo ' <a href="https://t.me/musecat77">t.me</a>';
-echo '</p>';
-echo MP::x('<p>Idea author:<br>');
-echo '<b>twsparkle</b> <a href="https://github.com/diller444">github</a>';
-echo ' <a href="https://t.me/twsparkle">t.me</a>';
-echo '</p>';
-echo '<p><small>';
-echo '</small></p>';
-echo Themes::bodyEnd();
+?>
+</p>
+<p>Developers:<br>
+<b>Shinovon</b> <a href="https://github.com/shinovon">github</a>
+ <a href="https://t.me/shinovon">t.me</a>
+<br>
+<b>MuseCat77</b></i>
+ <a href="https://github.com/musecat77">github</a>
+ <a href="https://t.me/musecat77">t.me</a>
+</p>
+<p>Idea author:<br>
+<b>twsparkle</b> <a href="https://github.com/diller444">github</a>
+ <a href="https://t.me/twsparkle">t.me</a>
+</p>
+<p>Donate:<br>
+4377723772807974 Tinkoff (RU)<br>
+4400430278594491 Kaspi (KZ)<br>
+<a href="https://boosty.to/nnproject/donate">boosty.to/nnproject/donate</a>
+</p>
+<?php echo Themes::bodyEnd(); ?>
