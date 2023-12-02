ed20765364bbc4bb9ce72693fb24bee26922eb91
Shinovon <arman200711@gmail.com>
2023-12-02 17:28:51 +0500

    Chat pagination fix

--- chat.php
+++ chat.php
@@ -333,7 +333,10 @@ setTimeout("location.reload(true);",'.$updint.'000);
 	echo '</div>';
 	if(!$reverse) {
 		if(count($rm) >= $msglimit) {
-			echo '<p><a href="'.$file.'?c='.$id.'&d=d&offset_from='.$rm[count($rm)-1]['id'].'">'.MP::x($lng['history_down']).'</a></p>';
+			if($endReached)
+				echo '<p><a href="'.$file.'?c='.$id.'&offset='.$msglimit.'&d=d">'.MP::x($lng['history_down']).'</a></p>';
+			else
+				echo '<p><a href="'.$file.'?c='.$id.'&d=d&offset_from='.$rm[count($rm)-1]['id'].'">'.MP::x($lng['history_down']).'</a></p>';
 		}
 	} else {
 		if($hasOffset && !$endReached) {
